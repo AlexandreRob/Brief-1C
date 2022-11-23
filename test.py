fff5be5dc764b10fd856c2ad6bd6136b117ef4e9
AlexandreRob <alexandre.robert974@hotmail.fr>
2022-11-23 16:10:05 +0400

    Init

--- test.py
+++ test.py
@@ -2,9 +2,15 @@ import pandas as pd
 
 df = pd.read_csv('data.csv', encoding= 'unicode_escape')
 
-df = df.dropna(axis=0) #Efface les null dans le tableau
-df = df.drop(['StockCode', 'Description', 'InvoiceDate'], axis=1) # Efface des colonne dans le tableau
+############################################################### nettoyer les données brutes (suppression des doublons et “faux”, standardisation, etc)
+df = df.drop(['CustomerID', 'InvoiceDate','Description'], axis=1) 
+#Ne pas oublier axis= 0 pour les lignes 1 pour les colonnes
+#Efface du tableau les colonnes CustomerID InvoiceDate et Description
 
-print(df.head(10))
+df.duplicated().sum() #nombre de doublon #La méthode duplicated() renvoie une valeur booléenne pour chaque ligne
 
+df.drop_duplicates(inplace= True) #efface les doublons
+############################################################### 
 
+############################################################### 
+#un graphique précisant la répartition des ventes par produit
